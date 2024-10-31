dfe769ff993e4a5725cd3cc7271e62b7224352be
Mao <code@mao.to>
2024-10-31 13:35:04 +0800

    update sha256 in brew formula

--- shorm.rb
+++ shorm.rb
@@ -6,14 +6,14 @@ class Shorm < Formula
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/laishulu/shorm/releases/download/v#{version}/shorm-macos-arm64"
-      sha256 "c36e31d2c66fbca0107dac41557cbebbf88d40830dde339505112771b82bd0ed"
+      sha256 "d46475f569523fb0f3b375d0036f7a455a96aa2128de8b71d78a3cac672f9f17"
     else
       url "https://github.com/laishulu/shorm/releases/download/v#{version}/shorm-macos-x86_64"
-      sha256 "501032f330412ff744961f9fd309d170cbe7646ec8c4368548bc4ab2848b99f7"
+      sha256 "273b518e624bac6cc75b35959f6248a3cc428fce211238108d0a82c2fea80524"
     end
   elsif OS.linux?
     url "https://github.com/laishulu/shorm/releases/download/v#{version}/shorm-linux-x86_64"
-    sha256 "eb9e21c2f5db244d61a9042833929113486d9cda4c8ae11f6062dd1d52fb4912"
+    sha256 "9ab0cfb4748d14fa7ff430a66a41fbfe30164096d7a9f93786bed8dd6d24f2f5"
   end
 
   def install
