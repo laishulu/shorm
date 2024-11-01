bac3e88fbbce0259745751e9bd24ecf2946b9215
Mao <code@mao.to>
2024-11-01 21:35:16 +0800

    update brew formula

--- shorm.rb
+++ shorm.rb
@@ -1,19 +1,19 @@
 class Shorm < Formula
   desc "SSH Connection Manager"
   homepage "https://github.com/laishulu/shorm"
-  version "1.0.0"
+  version "1.1.1"
 
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/laishulu/shorm/releases/download/v#{version}/shorm-macos-arm64"
-      sha256 "d46475f569523fb0f3b375d0036f7a455a96aa2128de8b71d78a3cac672f9f17"
+      sha256 "8d3a22214b7c96dca572bd98d7387f7f8e85090cd7a428e94c034344ab478f50"
     else
       url "https://github.com/laishulu/shorm/releases/download/v#{version}/shorm-macos-x86_64"
-      sha256 "273b518e624bac6cc75b35959f6248a3cc428fce211238108d0a82c2fea80524"
+      sha256 "412c6460d259380a42329d73ba7e9f4823cf5a017bdb8bd5b9427b0eb5130af1"
     end
   elsif OS.linux?
     url "https://github.com/laishulu/shorm/releases/download/v#{version}/shorm-linux-x86_64"
-    sha256 "9ab0cfb4748d14fa7ff430a66a41fbfe30164096d7a9f93786bed8dd6d24f2f5"
+    sha256 "ca00df6d98ccb2697849a0fe305184bb8b545e0a382dc966a64c71a4cefdccbe"
   end
 
   def install
